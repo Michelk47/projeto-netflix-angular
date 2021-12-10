88435c3221f00ee9fa50b4f6dd0dd1a75bda57d9
Michel de Alcantara <michel.alcantara@baraodemaua.br>
2021-12-10 17:46:18 -0300

    adicionado projeto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista';
+
+  menuOpened = false;
+
+  listTitles = [{
+    trailer:'/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 1',
+    relevance: 87,
+    parts: 2,
+    age: 0,
+    categorias: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+
+  },
+  {
+    trailer:'/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 74,
+    parts: 2,
+    age: 12,
+    categorias: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+
+  },
+  {
+    trailer:'/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 69,
+    parts: 2,
+    age: 14,
+    categorias: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+
+  },
+  {
+    trailer:'/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: '/assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 4',
+    relevance: 98,
+    parts: 2,
+    age: 16,
+    categorias:['Épico', 'Filme de fantasia', 'Viagem no espaço']
+
+  }
+];
+setMenuState(state: boolean){
+
+  this.menuOpened = state;
+
 }
+
+closeMenu() {
+  this.menuOpened = false;
+
+}
+
+}
+
+
---
+++ src/app/shared/title/title.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+  @Input() title: any;
+  @ViewChild('trailer', { static: true}) trailer: ElementRef<HTMLVideoElement>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  playVideo(){
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.play();
+
+  }
+
+  stopVideo(){
+    this.trailer.nativeElement.pause();
+
+  }
+
+}
